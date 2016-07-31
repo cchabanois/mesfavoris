e70086daea0a464c632dd7f0c55110ab8e8799fe
Cedric Chabanois <cchabanois@gmail.com>
2016-07-31 21:36:53 +0200

    Use SafeRunner to get bookmark properties

--- bundles/mesfavoris/src/mesfavoris/internal/bookmarktypes/BookmarkPropertiesProvider.java
+++ bundles/mesfavoris/src/mesfavoris/internal/bookmarktypes/BookmarkPropertiesProvider.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.ISafeRunnable;
+import org.eclipse.core.runtime.SafeRunner;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
 
+import mesfavoris.StatusHelper;
 import mesfavoris.bookmarktype.IBookmarkPropertiesProvider;
+import mesfavoris.bookmarktype.IGotoBookmark;
+import mesfavoris.model.Bookmark;
 
 public class BookmarkPropertiesProvider implements IBookmarkPropertiesProvider {
 	private final List<IBookmarkPropertiesProvider> providers;
@@ -19,7 +25,21 @@ public class BookmarkPropertiesProvider implements IBookmarkPropertiesProvider {
 	@Override
 	public void addBookmarkProperties(Map<String, String> bookmarkProperties, IWorkbenchPart part,
 			ISelection selection) {
-		providers.forEach(p -> p.addBookmarkProperties(bookmarkProperties, part, selection));
+		providers.forEach(p -> addBookmarkProperties(p, bookmarkProperties, part, selection));
+	}
+
+	private void addBookmarkProperties(IBookmarkPropertiesProvider provider, Map<String, String> bookmarkProperties,
+			IWorkbenchPart part, ISelection selection) {
+		SafeRunner.run(new ISafeRunnable() {
+
+			public void run() throws Exception {
+				provider.addBookmarkProperties(bookmarkProperties, part, selection);
+			}
+
+			public void handleException(Throwable exception) {
+				StatusHelper.logError("Error adding bookmark properties", exception);
+			}
+		});
 	}
 
 }
