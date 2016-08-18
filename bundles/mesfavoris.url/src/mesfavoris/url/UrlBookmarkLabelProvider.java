c2c4c4217bed6ffaf4c92cffd155c3a293fae376
Cedric Chabanois <cchabanois@gmail.com>
2016-08-18 23:23:07 +0200

    Always use 16x16 icons in UrlBookmarkLabelProvider.
    We resize the icon if necessary.

--- bundles/mesfavoris.url/src/mesfavoris/url/UrlBookmarkLabelProvider.java
+++ bundles/mesfavoris.url/src/mesfavoris/url/UrlBookmarkLabelProvider.java
@@ -4,14 +4,18 @@ import static mesfavoris.url.UrlBookmarkProperties.PROP_FAVICON;
 import static mesfavoris.url.UrlBookmarkProperties.PROP_URL;
 
 import java.io.ByteArrayInputStream;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Optional;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.resource.ResourceManager;
+import org.eclipse.swt.SWTException;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
 
 import mesfavoris.bookmarktype.AbstractBookmarkLabelProvider;
 import mesfavoris.model.Bookmark;
@@ -34,9 +38,36 @@ public class UrlBookmarkLabelProvider extends AbstractBookmarkLabelProvider {
 			return null;
 		}
 		byte[] favIconBytes = Base64.getDecoder().decode(favIconAsBase64);
-		ImageData imageData = new ImageData(new ByteArrayInputStream(favIconBytes));
-		ImageDescriptor imageDescriptor = ImageDescriptor.createFromImageData(imageData);
+		Optional<ImageData> imageData = getImageData(favIconBytes);
+		if (!imageData.isPresent()) {
+			return null;
+		}
+		ImageDescriptor imageDescriptor = ImageDescriptor.createFromImageData(imageData.get());
 		return resourceManager.createImage(imageDescriptor);
+	}
+
+	private Optional<ImageData> getImageData(byte[] favIconBytes) {
+		ImageData[] imageDatas;
+		try {
+			imageDatas = new ImageLoader().load(new ByteArrayInputStream(favIconBytes));
+		} catch (SWTException e) {
+			return Optional.empty();
+		}
+		Optional<ImageData> optionalImageData = Arrays.stream(imageDatas).sorted((imageData1,
+				imageData2) -> distanceFrom16x16ImageData(imageData1) - distanceFrom16x16ImageData(imageData2))
+				.findFirst();
+		if (!optionalImageData.isPresent()) {
+			return optionalImageData;
+		}
+		ImageData imageData = optionalImageData.get();
+		if (imageData.width <= 16 && imageData.height <= 16) {
+			return optionalImageData;
+		}
+		return Optional.of(imageData.scaledTo(16, 16));
+	}
+
+	private int distanceFrom16x16ImageData(ImageData imageData) {
+		return imageData.width * imageData.height - 16 * 16;
 	}
 
 	@Override
---
+++ tests/mesfavoris.url.tests/src/mesfavoris/url/UrlBookmarkLabelProviderTest.java
@@ -0,0 +1,42 @@
+package mesfavoris.url;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+
+import org.eclipse.swt.graphics.Image;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.ByteStreams;
+
+import mesfavoris.model.Bookmark;
+import mesfavoris.model.BookmarkId;
+
+public class UrlBookmarkLabelProviderTest {
+	private UrlBookmarkLabelProvider urlBookmarkLabelProvider = new UrlBookmarkLabelProvider();
+
+	@Test
+	public void testGetImageReturns16x16Images() throws IOException {
+		// Given
+		// lemonde-favicon.ico has 2 images in it : one 32x32 and one 16x16
+		Bookmark bookmark = new Bookmark(new BookmarkId(),
+				ImmutableMap.of(UrlBookmarkProperties.PROP_FAVICON, getImageAsBase64("lemonde-favicon.ico")));
+
+		// When
+		Image image = urlBookmarkLabelProvider.getImage(bookmark);
+		
+		// Then
+		assertEquals(16, image.getBounds().width);
+		assertEquals(16, image.getBounds().height);
+	}
+
+	private String getImageAsBase64(String resourceName) throws IOException {
+		try (InputStream is = getClass().getResourceAsStream(resourceName)) {
+			return Base64.getEncoder().encodeToString(ByteStreams.toByteArray(is));
+		}
+	}
+
+}
