b6066b9944a8841a0d021ea281d625b7a8aed4c2
Cedric Chabanois <cchabanois@gmail.com>
2016-08-21 13:44:51 +0200

    Added new test

---
+++ tests/mesfavoris.texteditor.tests/src/mesfavoris/texteditor/resource/FuzzyResourceFinderTest.java
@@ -0,0 +1,52 @@
+package mesfavoris.texteditor.resource;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+
+import mesfavoris.commons.ui.wizards.datatransfer.BundleProjectImportOperation;
+
+public class FuzzyResourceFinderTest {
+	private final FuzzyResourceFinder fuzzyResourceFinder = new FuzzyResourceFinder();
+	
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		importProjectFromTemplate("fuzzyResourceFinderTest", "commons-cli");
+	}
+	
+	@Test
+	public void testExactPath() {
+		// When
+		Optional<IResource> resource = fuzzyResourceFinder.find(new Path("/fuzzyResourceFinderTest/NOTICE.txt"), IResource.FILE);
+		
+		// Then
+		assertTrue(resource.isPresent());
+		assertEquals(new Path("/fuzzyResourceFinderTest/NOTICE.txt"), resource.get().getFullPath());
+	}
+
+	@Test
+	public void testFuzzyPath() {
+		// When
+		Optional<IResource> resource = fuzzyResourceFinder.find(new Path("/fuzzyResourceFinderTest/org/apache/commons/cli/CommandLine.java"), IResource.FILE);
+		
+		// Then
+		assertTrue(resource.isPresent());
+		assertEquals(new Path("/fuzzyResourceFinderTest/src/main/java/org/apache/commons/cli/CommandLine.java"), resource.get().getFullPath());
+	}	
+	
+	private static void importProjectFromTemplate(String projectName, String templateName)
+			throws InvocationTargetException, InterruptedException {
+		Bundle bundle = Platform.getBundle("mesfavoris.texteditor.tests");
+		new BundleProjectImportOperation(bundle, projectName, "/projects/" + templateName + "/").run(null);
+	}	
+	
+}
