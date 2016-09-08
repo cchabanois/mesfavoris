1baac40c3f350c50c0cfd4be98e52c3aec6b0f4c
Cedric Chabanois <cchabanois@gmail.com>
2016-09-08 23:18:30 +0200

    BookmarkModificationValidator now takes into account readonly property.

--- bundles/mesfavoris/src/mesfavoris/validation/BookmarkModificationValidator.java
+++ bundles/mesfavoris/src/mesfavoris/validation/BookmarkModificationValidator.java
@@ -5,13 +5,12 @@ import org.eclipse.core.runtime.Status;
 
 import mesfavoris.BookmarksPlugin;
 import mesfavoris.model.Bookmark;
-import mesfavoris.model.BookmarkDatabase;
 import mesfavoris.model.BookmarkId;
 import mesfavoris.model.BookmarksTree;
 import mesfavoris.remote.IRemoteBookmarksStore;
+import mesfavoris.remote.IRemoteBookmarksStore.State;
 import mesfavoris.remote.RemoteBookmarkFolder;
 import mesfavoris.remote.RemoteBookmarksStoreManager;
-import mesfavoris.remote.IRemoteBookmarksStore.State;
 
 public class BookmarkModificationValidator implements IBookmarkModificationValidator {
 	private final RemoteBookmarksStoreManager remoteBookmarksStoreManager;
@@ -33,13 +32,17 @@ public class BookmarkModificationValidator implements IBookmarkModificationValid
 		}
 		IRemoteBookmarksStore remoteBookmarksStore = remoteBookmarksStoreManager.getRemoteBookmarksStore(remoteBookmarkFolder.getRemoteBookmarkStoreId());
 		boolean connected = remoteBookmarksStore.getState() == State.connected;
-		if (connected) {
+		if (connected && !isReadOnly(remoteBookmarkFolder)) {
 			return Status.OK_STATUS;
 		} else {
-			return errorStatus("Cannot modify bookmark that is under a shared folder not connected");
+			return errorStatus("Cannot modify bookmark that is under a shared folder that is not connected or readonly");
 		}
 	}
 
+	private boolean isReadOnly(RemoteBookmarkFolder remoteBookmarkFolder) {
+		return Boolean.TRUE.toString().equalsIgnoreCase(remoteBookmarkFolder.getProperties().get(RemoteBookmarkFolder.PROP_READONLY));
+	}
+	
 	private IStatus errorStatus(String message) {
 		return new Status(Status.ERROR, BookmarksPlugin.PLUGIN_ID, message);
 	}
