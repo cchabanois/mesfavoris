bd341ed70294f4e0138b348994930395e59c2488
Cedric Chabanois <cchabanois@gmail.com>
2016-09-11 17:31:12 +0200

    Fixed test

--- tests/mesfavoris.gdrive.tests/src/mesfavoris/gdrive/operations/GetBookmarksFilesOperationTest.java
+++ tests/mesfavoris.gdrive.tests/src/mesfavoris/gdrive/operations/GetBookmarksFilesOperationTest.java
@@ -1,10 +1,12 @@
 package mesfavoris.gdrive.operations;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.junit.Rule;
@@ -19,36 +21,36 @@ public class GetBookmarksFilesOperationTest {
 
 	@Rule
 	public GDriveConnectionRule gdriveConnectionUser1 = new GDriveConnectionRule(GDriveTestUser.USER1, true);
-	
+
 	@Rule
 	public GDriveConnectionRule gdriveConnectionUser2 = new GDriveConnectionRule(GDriveTestUser.USER2, true);
-	
-	
+
 	@Test
 	public void testGetSharedBookmarkFile() throws Exception {
 		// Given
 		File file = createFile(gdriveConnectionUser1, "user1File.txt", "the contents");
 		share(gdriveConnectionUser1, file.getId(), GDriveTestUser.USER2.getEmail());
 		GetBookmarkFilesOperation operation = new GetBookmarkFilesOperation(gdriveConnectionUser2.getDrive());
-		
+
 		// When
 		List<File> bookmarkFiles = operation.getBookmarkFiles();
-		
+
 		// Then
-		assertEquals(1, bookmarkFiles.size());
-		assertEquals(file.getId(), bookmarkFiles.get(0).getId());
+		assertThat(bookmarkFiles.stream().map(f -> f.getId()).collect(Collectors.toList()))
+				.contains(file.getId());
 	}
 
-	private File createFile(GDriveConnectionRule driveConnection, String name, String contents) throws UnsupportedEncodingException, IOException {
+	private File createFile(GDriveConnectionRule driveConnection, String name, String contents)
+			throws UnsupportedEncodingException, IOException {
 		CreateFileOperation createFileOperation = new CreateFileOperation(driveConnection.getDrive());
 		File file = createFileOperation.createFile(driveConnection.getApplicationFolderId(), name,
 				contents.getBytes("UTF-8"), new NullProgressMonitor());
 		return file;
-	}	
-	
+	}
+
 	private void share(GDriveConnectionRule driveConnection, String fileId, String userEmail) throws IOException {
 		ShareFileOperation shareFileOperation = new ShareFileOperation(driveConnection.getDrive());
-		shareFileOperation.shareWithUser(fileId,userEmail, true);
+		shareFileOperation.shareWithUser(fileId, userEmail, true);
 	}
-	
+
 }
