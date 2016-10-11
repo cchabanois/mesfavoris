274657b4868f2980b866a441edd90741d7ed2c1c
Cedric Chabanois <cchabanois@gmail.com>
2016-10-11 22:50:09 +0200

    Improved JavadocCommentProvider

--- bundles/mesfavoris.java/src/mesfavoris/java/javadoc/JavadocCommentProvider.java
+++ bundles/mesfavoris.java/src/mesfavoris/java/javadoc/JavadocCommentProvider.java
@@ -1,49 +1,53 @@
 package mesfavoris.java.javadoc;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
+import java.util.stream.Collectors;
 
 import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.ui.JavadocContentAccess;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document.OutputSettings;
+import org.jsoup.safety.Whitelist;
 
 import com.google.common.io.CharStreams;
 
 public class JavadocCommentProvider {
 
 	public String getJavadocCommentShortDescription(IMember member) {
-		String javadocComment = getJavadocComment(member);
+		String javadocComment = getJavadocCommentAsText(member);
 		if (javadocComment == null) {
 			return null;
 		}
 		return getJavadocCommentShortDescription(javadocComment);
 	}
-	
-	public String getJavadocComment(IMember member) {
-		try (Reader reader = JavadocContentAccess.getContentReader(member, true)) {
+
+	private String getJavadocCommentAsText(IMember member) {
+		try (Reader reader = JavadocContentAccess.getHTMLContentReader(member, true, true)) {
 			if (reader == null) {
 				return null;
 			}
-			return CharStreams.toString(reader);
-		} catch (JavaModelException|IOException e) {
+			String javadocAsHtml = CharStreams.toString(reader);
+			String javadocAsString = Jsoup.clean(javadocAsHtml, "", Whitelist.none(), new OutputSettings().prettyPrint(false));
+			
+			// trim lines
+			try (BufferedReader bufferedReader = new BufferedReader(new StringReader(javadocAsString))) {
+				return bufferedReader.lines().map(line->line.trim()).collect(Collectors.joining("\n"));
+			}
+		} catch (JavaModelException | IOException e) {
 			return null;
 		}
-	}	
-	
-	public String getJavadocCommentShortDescription(String javadoc) {
+	}
+
+	private String getJavadocCommentShortDescription(String javadoc) {
 		int index = javadoc.indexOf("\n\n");
 		if (index != -1) {
 			javadoc = javadoc.substring(0, index);
 		}
-		index = javadoc.indexOf("<p>");
-		if (index != -1) {
-			javadoc = javadoc.substring(0, index);
-		}
-		javadoc = javadoc.replaceAll("<[a-zA-Z]*>", "");
-		javadoc = javadoc.replaceAll("</[a-zA-Z]*>", "");
-		javadoc = javadoc.replaceAll("\\{@inheritDoc\\}", "");
-		javadoc = javadoc.replaceAll("\\{@(link|value|code|literal) (.*)\\}", "$2");
 		return javadoc;
 	}
-	
+
 }
--- tests/mesfavoris.java.tests/src/mesfavoris/java/javadoc/JavadocCommentProviderTest.java
+++ tests/mesfavoris.java.tests/src/mesfavoris/java/javadoc/JavadocCommentProviderTest.java
@@ -1,16 +1,30 @@
 package mesfavoris.java.javadoc;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
+import org.eclipse.jdt.core.IBuffer;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IOpenable;
+import org.eclipse.jdt.core.SourceRange;
+import org.eclipse.jdt.internal.core.BufferManager;
+import org.eclipse.jdt.internal.core.JavaElement;
 import org.junit.Test;
 
 public class JavadocCommentProviderTest {
 	private JavadocCommentProvider javadocCommentProvider = new JavadocCommentProvider();
 
 	@Test
-	public void testJavadocCommentShortDescription() {
+	public void testJavadocCommentShortDescription() throws Exception {
 		// Given
-		String javadoc = "Query to see if this Option requires an argument\n\n@return boolean flag indicating if an argument is required";
+		String javadoc = 
+				  "/**\n "
+				+ " * Query to see if this Option requires an argument\n"
+				+ " * \n"
+				+ " * @return boolean flag indicating if an argument is required\n"
+				+ " */";
 		
 		// When
 		String shortDescription = shortDescription(javadoc);
@@ -20,33 +34,68 @@ public class JavadocCommentProviderTest {
 	}
 
 	@Test
-	public void testJavadocCommentWithHtmlTags() {
+	public void testJavadocCommentWithHtmlTags() throws Exception {
 		// Given
-		String javadoc = "Returns the specified value of this Option or \n"
-				+ "<code>null</code> if there is no value.\n" + "\n"
-				+ "@return the value/first value of this Option or \n" + " <code>null</code> if there is no value.";
+		String javadoc = 
+				  "/**\n "
+				+ " * Returns the specified value of this Option or \n"
+				+ " * <code>null</code> if there is no value.\n"
+				+ " * \n"
+				+ " * @return the value/first value of this Option or \n"
+				+ " * <code>null</code> if there is no value.\n"
+				+ " */";
 		// When
 		String shortDescription = shortDescription(javadoc);
 
 		// Then
-		assertEquals("Returns the specified value of this Option or \nnull if there is no value.", shortDescription);
+		assertEquals("Returns the specified value of this Option or\nnull if there is no value.", shortDescription);
 	}
 	
 	@Test
-	public void testJavadocCommentWithTags() {
-		String javadoc = "Returns the specified value of this Option or \n"
-				+ "{@code null} if there is no value.\n" + "\n"
-				+ "@return the value/first value of this Option or \n" + " <code>null</code> if there is no value.";
+	public void testJavadocCommentWithTags() throws Exception {
+		// Given
+		String javadoc = 
+				  "/**\n "
+				+ " * Returns the specified value of this Option or \n"
+				+ " * {@code null} if there is no value.\n"
+				+ " * \n"
+				+ " * @return the value/first value of this Option or \n"
+				+ " * <code>null</code> if there is no value.\n"
+				+ " */";
 		// When
 		String shortDescription = shortDescription(javadoc);
 
 		// Then
-		assertEquals("Returns the specified value of this Option or \nnull if there is no value.", shortDescription);		
+		assertEquals("Returns the specified value of this Option or\nnull if there is no value.", shortDescription);		
 		
 	}
 	
-	private String shortDescription(String javadoc) {
-		return javadocCommentProvider.getJavadocCommentShortDescription(javadoc);
+	@Test
+	public void testJavadocWithEncodedCharacters() throws Exception {
+		// Given
+		String javadoc = 
+				  "/**\n "
+				+ " * A program element annotated &#64;Deprecated is one that programmers are discouraged from using\n"
+			    + " */";
+	
+		// When
+		String shortDescription = shortDescription(javadoc);
+
+		// Then
+		assertEquals("A program element annotated @Deprecated is one that programmers are discouraged from using", shortDescription);		
+
+		
+	}
+	
+	private String shortDescription(String javadoc) throws Exception {
+		IMember member = mock(IMember.class);
+		IOpenable openable = (IOpenable)mock(JavaElement.class, withSettings().extraInterfaces(IOpenable.class));
+		when(member.getOpenable()).thenReturn(openable);
+		IBuffer buffer = BufferManager.createBuffer(openable);
+		buffer.setContents(javadoc);
+		when(openable.getBuffer()).thenReturn(buffer);
+		when(member.getJavadocRange()).thenReturn(new SourceRange(0, javadoc.length()));
+		return javadocCommentProvider.getJavadocCommentShortDescription(member);
 	}
 
 }
