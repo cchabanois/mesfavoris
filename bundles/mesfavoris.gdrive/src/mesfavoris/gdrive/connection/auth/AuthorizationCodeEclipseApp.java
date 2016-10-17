727e68b5c8a22b29774883fb22724bcf7c04c0e5
Cedric Chabanois <cchabanois@gmail.com>
2016-10-17 23:14:31 +0200

    Copy the authorization url in clipboard

--- bundles/mesfavoris.gdrive/src/mesfavoris/gdrive/connection/auth/AuthorizationCodeEclipseApp.java
+++ bundles/mesfavoris.gdrive/src/mesfavoris/gdrive/connection/auth/AuthorizationCodeEclipseApp.java
@@ -5,6 +5,10 @@ import java.net.MalformedURLException;
 import java.net.URI;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.browser.IWebBrowser;
@@ -30,11 +34,25 @@ public class AuthorizationCodeEclipseApp extends AuthorizationCodeInstalledApp {
 	protected void onAuthorization(
 			AuthorizationCodeRequestUrl authorizationUrl)
 			throws IOException {
-		monitor.subTask("Please open the following address in your browser:"
-				+ authorizationUrl);
-		openInExternalBrowser(URI.create(authorizationUrl.build()));
+		String authorizationUrlAsString = authorizationUrl.build();
+		Display display = Display.getDefault();
+		display.syncExec(() -> copyTextToClipboard(display, authorizationUrlAsString));
+		monitor.subTask("Please authorize the application in your browser. Url has been copied to clipboard");		
+		openInExternalBrowser(URI.create(authorizationUrlAsString));
 	}
 
+	private void copyTextToClipboard(Display display, String text) {
+		Clipboard clipboard = new Clipboard(display);
+		try {
+			TextTransfer textTransfer = TextTransfer.getInstance();
+			Transfer[] transfers = new Transfer[] { textTransfer };
+			Object[] data = new Object[] { text };
+			clipboard.setContents(data, transfers);
+		} finally {
+			clipboard.dispose();
+		}
+	}	
+	
 	private void openInExternalBrowser(URI url) {
 		String browserId = "gdrive-authorize";
 		try {
