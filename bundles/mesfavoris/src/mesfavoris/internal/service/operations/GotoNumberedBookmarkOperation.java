ae98278bfa8c19601acb40ed9cc7e1890f70fa20
Cedric Chabanois <cchabanois@gmail.com>
2016-10-24 22:01:57 +0200

    It is now possible to go to a numbered bookmark folder

--- bundles/mesfavoris/src/mesfavoris/internal/service/operations/GotoNumberedBookmarkOperation.java
+++ bundles/mesfavoris/src/mesfavoris/internal/service/operations/GotoNumberedBookmarkOperation.java
@@ -3,6 +3,10 @@ package mesfavoris.internal.service.operations;
 import java.util.Optional;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 
 import mesfavoris.BookmarksException;
 import mesfavoris.bookmarktype.IBookmarkLocationProvider;
@@ -10,6 +14,7 @@ import mesfavoris.bookmarktype.IGotoBookmark;
 import mesfavoris.internal.numberedbookmarks.BookmarkNumber;
 import mesfavoris.internal.numberedbookmarks.NumberedBookmarks;
 import mesfavoris.model.BookmarkDatabase;
+import mesfavoris.model.BookmarkFolder;
 import mesfavoris.model.BookmarkId;
 
 public class GotoNumberedBookmarkOperation {
@@ -17,7 +22,7 @@ public class GotoNumberedBookmarkOperation {
 	private final BookmarkDatabase bookmarkDatabase;
 	private final IBookmarkLocationProvider bookmarkLocationProvider;
 	private final IGotoBookmark gotoBookmark;
-	
+
 	public GotoNumberedBookmarkOperation(NumberedBookmarks numberedBookmarks, BookmarkDatabase bookmarkDatabase,
 			IBookmarkLocationProvider bookmarkLocationProvider, IGotoBookmark gotoBookmark) {
 		this.numberedBookmarks = numberedBookmarks;
@@ -26,14 +31,38 @@ public class GotoNumberedBookmarkOperation {
 		this.gotoBookmark = gotoBookmark;
 	}
 
-	public void gotoNumberedBookmark(BookmarkNumber bookmarkNumber, IProgressMonitor monitor) throws BookmarksException {
+	public void gotoNumberedBookmark(BookmarkNumber bookmarkNumber, IProgressMonitor monitor)
+			throws BookmarksException {
 		Optional<BookmarkId> bookmarkId = numberedBookmarks.getBookmark(bookmarkNumber);
 		if (!bookmarkId.isPresent()) {
 			throw new BookmarksException("Bookmark does not exist anymore");
 		}
+		if (bookmarkDatabase.getBookmarksTree().getBookmark(bookmarkId.get()) instanceof BookmarkFolder) {
+			gotoBookmarkFolder(bookmarkId.get());
+		} else {
+			gotoBookmark(bookmarkId.get(), monitor);
+		}
+	}
+
+	private void gotoBookmarkFolder(BookmarkId bookmarkId) {
+		Display.getDefault().asyncExec(() -> {
+			ShowInBookmarksViewOperation operation = new ShowInBookmarksViewOperation(bookmarkDatabase);
+			IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+			if (workbenchWindow == null) {
+				return;
+			}
+			IWorkbenchPage page = workbenchWindow.getActivePage();
+			if (page == null) {
+				return;
+			}
+			operation.showInBookmarksView(page, bookmarkId, true);
+		});
+	}
+
+	private void gotoBookmark(BookmarkId bookmarkId, IProgressMonitor monitor) throws BookmarksException {
 		GotoBookmarkOperation operation = new GotoBookmarkOperation(bookmarkDatabase, bookmarkLocationProvider,
 				gotoBookmark);
-		operation.gotoBookmark(bookmarkId.get(), monitor);
+		operation.gotoBookmark(bookmarkId, monitor);
 	}
 
 }
