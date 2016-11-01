62b2a0db493b6a0429c4e359afd303e2f26790fc
Cedric Chabanois <cchabanois@gmail.com>
2016-11-01 14:37:33 +0100

    Use progress service when importing bookmarks from gDrive

--- bundles/mesfavoris.gdrive/src/mesfavoris/gdrive/handlers/ImportBookmarksFromGdriveHandler.java
+++ bundles/mesfavoris.gdrive/src/mesfavoris/gdrive/handlers/ImportBookmarksFromGdriveHandler.java
@@ -1,22 +1,25 @@
 package mesfavoris.gdrive.handlers;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.progress.IProgressService;
 
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 
 import mesfavoris.BookmarksException;
 import mesfavoris.BookmarksPlugin;
+import mesfavoris.StatusHelper;
 import mesfavoris.gdrive.Activator;
 import mesfavoris.gdrive.connection.GDriveConnectionManager;
 import mesfavoris.gdrive.dialogs.ImportBookmarksFileDialog;
@@ -49,19 +52,31 @@ public class ImportBookmarksFromGdriveHandler extends AbstractHandler {
 		if (drive == null) {
 			return null;
 		}
-		ImportBookmarksFileDialog dialog = new ImportBookmarksFileDialog(shell, gDriveConnectionManager.getDrive(),
+		ImportBookmarksFileDialog dialog = new ImportBookmarksFileDialog(shell, drive,
 				gDriveConnectionManager.getApplicationFolderId(), bookmarkMappings);
 		if (dialog.open() == Dialog.CANCEL) {
 			return null;
 		}
 		File file = dialog.getFile();
-		ImportBookmarkFileOperation importBookmarkFileOperation = new ImportBookmarkFileOperation(drive,
-				bookmarkMappingsStore, bookmarksService, Optional.of(gDriveConnectionManager.getApplicationFolderId()));
+
+		IProgressService progressService = PlatformUI.getWorkbench().getProgressService();
 		try {
-			importBookmarkFileOperation.importBookmarkFile(bookmarkFolder.getId(), file.getId(),
-					new NullProgressMonitor());
-		} catch (BookmarksException | IOException e) {
-			throw new ExecutionException("Could not import bookmarks file", e);
+			progressService.busyCursorWhile(monitor -> {
+				ImportBookmarkFileOperation importBookmarkFileOperation = new ImportBookmarkFileOperation(drive,
+						bookmarkMappingsStore, bookmarksService,
+						Optional.of(gDriveConnectionManager.getApplicationFolderId()));
+				try {
+					importBookmarkFileOperation.importBookmarkFile(bookmarkFolder.getId(), file.getId(),
+							monitor);
+				} catch (BookmarksException | IOException e) {
+					throw new InvocationTargetException(e);
+				}
+
+			});
+		} catch (InvocationTargetException e) {
+			StatusHelper.showError("Could not import bookmarks file", e.getCause(), false);
+		} catch (InterruptedException e) {
+			throw new ExecutionException("Could not import bookmarks file : cancelled");
 		}
 		return null;
 	}
