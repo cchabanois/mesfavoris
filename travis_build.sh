31b9ae2df5d0c9b775e491bf5bd94232d31cd8ac
Cedric Chabanois <cchabanois@gmail.com>
2016-11-04 22:50:00 +0100

    .travis.yml now delegate to shell script

---
+++ travis_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ev
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+	echo "Build for Pull Request"
+	mvn clean verify
+elif [ $TRAVIS_TAG =~ ^release.*$ ]; then
+	echo "Build for release"
+	mvn clean verify -P deploy,release
+else
+	echo "Build for standard commit"
+	mvn clean verify -P deploy
+fi
