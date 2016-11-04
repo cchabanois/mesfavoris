d8bbd43c340c3ae039d7b765b310db0cd2829620
Cedric Chabanois <cchabanois@gmail.com>
2016-11-04 23:04:48 +0100

    Use the double bracket conditional compound command

--- travis_build.sh
+++ travis_build.sh
@@ -2,10 +2,10 @@
 
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
 	echo "Build for Pull Request"
 	mvn clean verify
-elif [ $TRAVIS_TAG =~ ^release.*$ ]; then
+elif [[ $TRAVIS_TAG =~ ^release.*$ ]]; then
 	echo "Build for release"
 	mvn clean verify -P deploy,release
 else
