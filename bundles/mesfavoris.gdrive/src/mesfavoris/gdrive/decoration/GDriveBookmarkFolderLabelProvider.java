d1c6091afe1f30bc05c5abe6d965bf11d02494e3
Cedric Chabanois <cchabanois@gmail.com>
2016-11-09 09:49:28 +0100

    forgot to add label provider for GDrive bookmark folders.

---
+++ bundles/mesfavoris.gdrive/src/mesfavoris/gdrive/decoration/GDriveBookmarkFolderLabelProvider.java
@@ -0,0 +1,51 @@
+package mesfavoris.gdrive.decoration;
+
+import java.util.Optional;
+
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.jface.viewers.StyledString.Styler;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+
+import mesfavoris.gdrive.Activator;
+import mesfavoris.gdrive.mappings.BookmarkMapping;
+import mesfavoris.gdrive.mappings.IBookmarkMappings;
+import mesfavoris.model.Bookmark;
+import mesfavoris.model.BookmarkFolder;
+import mesfavoris.model.BookmarkId;
+import mesfavoris.viewers.BookmarkFolderLabelProvider;
+import mesfavoris.viewers.StylerProvider;
+
+public class GDriveBookmarkFolderLabelProvider extends BookmarkFolderLabelProvider {
+	private final IBookmarkMappings bookmarkMappings;
+	private final StylerProvider stylerProvider = new StylerProvider();
+
+	public GDriveBookmarkFolderLabelProvider() {
+		bookmarkMappings = Activator.getBookmarkMappingsStore();
+	}
+
+	@Override
+	public StyledString getStyledText(Object element) {
+		StyledString styledString = super.getStyledText(element);
+		BookmarkFolder bookmarkFolder = (BookmarkFolder) element;
+		BookmarkId bookmarkId = bookmarkFolder.getId();
+		Optional<BookmarkMapping> bookmarkMapping = bookmarkMappings.getMapping(bookmarkId);
+		if (!bookmarkMapping.isPresent()) {
+			return styledString;
+		}
+		String sharingUser = bookmarkMapping.get().getProperties().get("sharingUser");
+		if (sharingUser == null) {
+			return styledString;
+		}
+		Styler styler = stylerProvider.getStyler(null,
+				Display.getCurrent().getSystemColor(SWT.COLOR_DARK_YELLOW), null);
+		styledString.append(String.format(" [Shared by %s]", sharingUser), styler);
+		return styledString;
+	}
+
+	@Override
+	public boolean handlesBookmark(Bookmark bookmark) {
+		return super.handlesBookmark(bookmark) && bookmarkMappings.getMapping(bookmark.getId()).isPresent();
+	}
+
+}
