01fd0cc49fcebcf0f5fe049dc8a5e8453fd7de68
Cedric Chabanois <cchabanois@gmail.com>
2016-11-15 14:05:31 +0100

    get test username from env

--- tests/mesfavoris.gdrive.tests/src/mesfavoris/gdrive/GDriveTestUser.java
+++ tests/mesfavoris.gdrive.tests/src/mesfavoris/gdrive/GDriveTestUser.java
@@ -4,27 +4,25 @@ import java.util.Optional;
 
 public enum GDriveTestUser {
 
-	USER1("mesfavoris.test"),
-	USER2("mesfavoris.test2");
-	
-	private final String userName;
-	
-	private GDriveTestUser(String userName) {
-		this.userName = userName;
-	}
+	USER1,
+	USER2;
 	
 	public String getUserName() {
+		String userName = System.getenv(name()+"_GDRIVE_USERNAME");
+		if (userName == null) {
+			throw new IllegalStateException("Could not get test username from env for "+name());
+		}
 		return userName;
 	}
 	
 	public String getEmail() {
-		return userName+"@gmail.com";
+		return getUserName()+"@gmail.com";
 	}
 	
 	public String getPassword() {
 		String password = System.getenv(name() +"_GDRIVE_PASSWORD");
 		if (password == null) {
-			throw new IllegalStateException("Could not get test password from env for "+getEmail());
+			throw new IllegalStateException("Could not get test password from env for "+name());
 		}
 		return password;
 	}
