2aa5503f263a0754f19e80aaa30d875d7f2f2dcc
Cedric Chabanois <cchabanois@gmail.com>
2016-12-13 12:14:09 +0100

    renamed file

--- bundles/mesfavoris.perforce/src/mesfavoris/perforce/PerforceBookmarkProperties.java
+++ bundles/mesfavoris.perforce/src/mesfavoris/perforce/PerforceBookmarkProperties.java
@@ -1,6 +1,6 @@
 package mesfavoris.perforce;
 
-public class PerforceProjectProperties {
+public class PerforceBookmarkProperties {
 	public static final String PROP_PORT = "p4.port";
 	public static final String PROP_PATH = "p4.path";
 	public static final String PROP_CHANGELIST = "p4.changelist";
