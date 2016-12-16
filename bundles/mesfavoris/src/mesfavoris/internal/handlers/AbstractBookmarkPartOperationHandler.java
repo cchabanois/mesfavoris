dbe5881b7859196085864e7bb15b42220fa1904a
Cedric Chabanois <cchabanois@gmail.com>
2016-12-16 17:45:15 +0100

    current selection offset is not always updated if selection length is 0

--- bundles/mesfavoris/src/mesfavoris/internal/handlers/AbstractBookmarkPartOperationHandler.java
+++ bundles/mesfavoris/src/mesfavoris/internal/handlers/AbstractBookmarkPartOperationHandler.java
@@ -12,16 +12,23 @@ public abstract class AbstractBookmarkPartOperationHandler extends AbstractBookm
 
 	protected BookmarkPartOperationContext getOperationContext(ExecutionEvent event) {
 		IWorkbenchPart part = HandlerUtil.getActivePart(event);
-		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		ISelection selection;
+		if (part != null) {
+			// see bug #375220 or #282969 to understand why we don't use
+			// HandlerUtil.getCurrentSelection(event);
+			selection = part.getSite().getSelectionProvider().getSelection();
+		} else {
+			selection = HandlerUtil.getCurrentSelection(event);
+		}
 		if (part instanceof BookmarksView) {
-			part = ((BookmarksView)part).getPreviousActivePart();
+			part = ((BookmarksView) part).getPreviousActivePart();
 			if (part != null) {
 				selection = part.getSite().getSelectionProvider().getSelection();
 			}
 		}
 		return new BookmarkPartOperationContext(part, selection);
-	}	
-	
+	}
+
 	protected static class BookmarkPartOperationContext {
 		protected final IWorkbenchPart part;
 		protected final ISelection selection;
