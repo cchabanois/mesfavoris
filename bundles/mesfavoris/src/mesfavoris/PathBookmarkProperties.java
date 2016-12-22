50ce901ecadf2ff2f3113bc2b22d0719be1f773d
Cedric Chabanois <cchabanois@gmail.com>
2016-12-22 14:43:01 +0100

    Was not building because of this missing file

---
+++ bundles/mesfavoris/src/mesfavoris/PathBookmarkProperties.java
@@ -0,0 +1,7 @@
+package mesfavoris;
+
+public class PathBookmarkProperties {
+	public static final String PROP_FILE_PATH = "filePath";
+	public static final String PROP_FOLDER_PATH = "folderPath";
+	public static final String PROP_WORKSPACE_PATH = "workspacePath";
+}
