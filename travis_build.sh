bb2aeb4178420073e1ea3778b96305753f0dba8a
Cedric Chabanois <cchabanois@gmail.com>
2017-01-24 22:01:52 +0100

    do not run tests when creating a release
    [ci skip]

--- travis_build.sh
+++ travis_build.sh
@@ -7,7 +7,8 @@ if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
 	mvn clean verify
 elif [[ $TRAVIS_TAG =~ ^release.*$ ]]; then
 	echo "Build for release"
-	mvn clean verify -P deploy,release
+	# skip tests, you are not supposed to add code when creating a release, just add a tag
+	mvn clean verify -P deploy,release -Dmaven.test.skip=true
 else
 	echo "Build for standard commit"
 	mvn clean verify -P deploy
