572b3d762ab0a0ab70094daeb4f5f18e17f37844
Cedric Chabanois <cchabanois@gmail.com>
2017-01-25 14:19:20 +0100

    do not publish to codecov for releases

--- travis_build.sh
+++ travis_build.sh
@@ -5,6 +5,10 @@ set -ev
 if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
 	echo "Build for Pull Request"
 	mvn clean verify
+	STATUS=$?
+	if [ $STATUS -eq 0 ]; then
+		bash <(curl -s https://codecov.io/bash)
+	fi
 elif [[ $TRAVIS_TAG =~ ^release.*$ ]]; then
 	echo "Build for release"
 	# skip tests, you are not supposed to add code when creating a release, just add a tag
@@ -12,4 +16,8 @@ elif [[ $TRAVIS_TAG =~ ^release.*$ ]]; then
 else
 	echo "Build for standard commit"
 	mvn clean verify -P deploy
+	STATUS=$?
+	if [ $STATUS -eq 0 ]; then
+		bash <(curl -s https://codecov.io/bash)
+	fi
 fi
