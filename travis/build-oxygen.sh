0b75c96e62f46224e951cb2b60a28e1bf5f3dac5
Cedric Chabanois <cchabanois@gmail.com>
2017-05-13 15:55:37 +0200

    Use maven wrapper in travis files

--- travis/build-oxygen.sh
+++ travis/build-oxygen.sh
@@ -4,10 +4,10 @@ set -ev
 
 if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
 	echo "Build for Pull Request"
-	mvn clean verify -P target-oxygen
+	./mvnw clean verify -P target-oxygen
 elif [[ $TRAVIS_TAG =~ ^release.*$ ]]; then
 	echo "Build for release : do nothing"
 else
 	echo "Build for standard commit"
-	mvn clean verify -P target-oxygen
+	./mvnw clean verify -P target-oxygen
 fi
