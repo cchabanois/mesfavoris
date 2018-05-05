f07fdae6d01427775073e730b2f3b9f8cad33bc1
Chabanois Cédric <cchabanois@gmail.com>
2018-05-05 22:23:04 +0200

    Fix git tests windows (#87)
    Fixed test failures on Windows

--- tests/mesfavoris.git.tests/src/mesfavoris/git/CommitEditorBookmarkPropertiesProviderTest.java
+++ tests/mesfavoris.git.tests/src/mesfavoris/git/CommitEditorBookmarkPropertiesProviderTest.java
@@ -6,14 +6,20 @@ import static mesfavoris.git.GitTestHelper.importGitProject;
 import static mesfavoris.tests.commons.ui.SWTBotViewHelper.closeWelcomeView;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.egit.core.Activator;
+import org.eclipse.egit.core.GitCorePreferences;
 import org.eclipse.egit.core.RepositoryCache;
 import org.eclipse.egit.ui.internal.commit.CommitEditor;
 import org.eclipse.egit.ui.internal.commit.RepositoryCommit;
@@ -22,20 +28,26 @@ import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
-import mesfavoris.BookmarksException;
+import mesfavoris.git.rules.TemporaryDefaultRepositoryDirRule;
 import mesfavoris.model.Bookmark;
 
 public class CommitEditorBookmarkPropertiesProviderTest {
 	private static final String REPOSITORY_URL = "https://github.com/LorenzoBettini/junit-swtbot-example.git";
 	private static final String PROJECT_NAME = "mathutils.core";
+
+	@Rule
+	public TemporaryDefaultRepositoryDirRule tempRepositoryDir = new TemporaryDefaultRepositoryDirRule();
+
 	private SWTWorkbenchBot bot;
 	private Map<String, String> bookmarkProperties = new HashMap<>();
 
 	@Before
-	public void setUp() throws BookmarksException {
+	public void setUp() throws Exception {
 		bot = new SWTWorkbenchBot();
 		closeWelcomeView();
 		importGitProject(REPOSITORY_URL, PROJECT_NAME);
--- tests/mesfavoris.git.tests/src/mesfavoris/git/ImportGitProjectTest.java
+++ tests/mesfavoris.git.tests/src/mesfavoris/git/ImportGitProjectTest.java
@@ -3,7 +3,6 @@ package mesfavoris.git;
 import static mesfavoris.git.GitBookmarkProperties.PROP_BRANCH;
 import static mesfavoris.git.GitBookmarkProperties.PROP_PROJECT_PATH;
 import static mesfavoris.git.GitBookmarkProperties.PROP_URL;
-import static mesfavoris.git.GitTestHelper.tryDeleteRepository;
 import static mesfavoris.tests.commons.bookmarks.BookmarkBuilder.bookmark;
 import static org.junit.Assert.assertTrue;
 
@@ -12,11 +11,12 @@ import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import mesfavoris.BookmarksException;
+import mesfavoris.git.rules.TemporaryDefaultRepositoryDirRule;
 import mesfavoris.model.Bookmark;
 
 public class ImportGitProjectTest {
@@ -24,17 +24,12 @@ public class ImportGitProjectTest {
 	private static final String PROJECT_NAME = "mathutils.core";
 	private ImportGitProject importGitProject;
 
+	@Rule
+	public TemporaryDefaultRepositoryDirRule tempRepositoryDir = new TemporaryDefaultRepositoryDirRule();	
+	
 	@Before
 	public void setUp() {
 		importGitProject = new ImportGitProject();
-		tryDeleteProject(PROJECT_NAME);
-		tryDeleteRepository(REPOSITORY_URL);
-	}
-
-	@After
-	public void tearDown() {
-		tryDeleteProject(PROJECT_NAME);
-		tryDeleteRepository(REPOSITORY_URL);
 	}
 
 	@Test
@@ -67,16 +62,6 @@ public class ImportGitProjectTest {
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IProject project = workspace.getRoot().getProject(PROJECT_NAME);
 		assertTrue(project.exists());
-	}
-
-	private void tryDeleteProject(String projectName) {
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IProject project = workspace.getRoot().getProject(projectName);
-		try {
-			project.delete(false, true, new NullProgressMonitor());
-		} catch (CoreException e) {
-
-		}
 	}
 
 }
