ff4852cfbdcc9c0c22df2554e84fd229dfb63e09
Chabanois Cédric <cchabanois@gmail.com>
2021-05-14 22:03:54 +0200

    Modified to publish the eclipse repository on github pages (#115)
    bintray is no more

--- travis/build-2019-09.sh
+++ travis/build-2019-09.sh
@@ -12,10 +12,10 @@ if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
 elif [[ $TRAVIS_TAG =~ ^release.*$ ]]; then
 	echo "Build for release"
 	# skip tests, you are not supposed to add code when creating a release, just add a tag
-	./mvnw clean verify -P deploy,release -Dmaven.test.skip=true
+	./mvnw clean deploy -P target-2019-09,release-composite -Dmaven.test.skip=true -Dgithub-update-repo=https://x-access-token:${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/cchabanois/mesfavoris-updates
 elif [[ ${TRAVIS_BRANCH} = "master" ]]; then 
 	echo "Build for standard commit on master"
-	./mvnw clean verify -P target-2019-09,jacoco-report,deploy
+	./mvnw clean verify -P target-2019-09,jacoco-report
 	STATUS=$?
 	if [ $STATUS -eq 0 ]; then
 		bash <(curl -s https://codecov.io/bash)
