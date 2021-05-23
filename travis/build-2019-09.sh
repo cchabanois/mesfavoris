a6449f3a8f430db4ec9a3981683096d859f331d9
Chabanois Cédric <cchabanois@gmail.com>
2021-05-23 23:08:07 +0200

    Added --fail-at-end to mvn parameters (#117)

--- travis/build-2019-09.sh
+++ travis/build-2019-09.sh
@@ -4,7 +4,7 @@ set -ev
 
 if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
 	echo "Build for Pull Request"
-	./mvnw clean verify -P target-2019-09,jacoco-report
+	./mvnw clean verify -P target-2019-09,jacoco-report --fail-at-end
 	STATUS=$?
 	if [ $STATUS -eq 0 ]; then
 		bash <(curl -s https://codecov.io/bash)
@@ -15,14 +15,14 @@ elif [[ $TRAVIS_TAG =~ ^release.*$ ]]; then
 	./mvnw clean deploy -P target-2019-09,release-composite -Dmaven.test.skip=true -Dgithub-update-repo=https://x-access-token:${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/cchabanois/mesfavoris-updates
 elif [[ ${TRAVIS_BRANCH} = "master" ]]; then 
 	echo "Build for standard commit on master"
-	./mvnw clean verify -P target-2019-09,jacoco-report
+	./mvnw clean verify -P target-2019-09,jacoco-report --fail-at-end
 	STATUS=$?
 	if [ $STATUS -eq 0 ]; then
 		bash <(curl -s https://codecov.io/bash)
 	fi
 else
 	echo "Build for commit on a branch"
-	./mvnw clean verify -P target-2019-09,jacoco-report
+	./mvnw clean verify -P target-2019-09,jacoco-report --fail-at-end
 	STATUS=$?
 	if [ $STATUS -eq 0 ]; then
 		bash <(curl -s https://codecov.io/bash)
