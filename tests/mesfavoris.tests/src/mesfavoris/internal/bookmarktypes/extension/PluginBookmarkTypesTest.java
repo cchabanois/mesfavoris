13f3f3ffa64c5c9b32fd3f87fcd47d412e8fc0b1
Cedric Chabanois <cchabanois@gmail.com>
2022-08-28 21:13:49 +0200

    Removed GithubUrlBookmarkPropertiesProvider from plugin.xml #145

--- tests/mesfavoris.tests/src/mesfavoris/internal/bookmarktypes/extension/PluginBookmarkTypesTest.java
+++ tests/mesfavoris.tests/src/mesfavoris/internal/bookmarktypes/extension/PluginBookmarkTypesTest.java
@@ -4,15 +4,26 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.internal.filebuffers.ResourceTextFileBufferManager;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.ILogListener;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.junit.Before;
 import org.junit.Test;
 
 import mesfavoris.bookmarktype.BookmarkPropertyDescriptor;
 import mesfavoris.bookmarktype.BookmarkPropertyDescriptor.BookmarkPropertyType;
 import mesfavoris.bookmarktype.IGotoBookmark;
+import mesfavoris.internal.BookmarksPlugin;
 import mesfavoris.internal.bookmarktypes.extension.PluginBookmarkType.PrioritizedElement;
 import mesfavoris.internal.markers.GotoFileMarkerBookmark;
 import mesfavoris.model.Bookmark;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PluginBookmarkTypesTest {
 	private PluginBookmarkTypes pluginBookmarkTypes;
@@ -46,6 +57,55 @@ public class PluginBookmarkTypesTest {
 		// Then
 		assertEquals(BookmarkPropertyType.STRING, propertyDescriptor.getType());
 		assertFalse(propertyDescriptor.isUpdatable());
+	}
+
+	@Test
+	public void testNoErrorOrWarningLogWhileLoadingPropertiesProviders() {
+		assertNoErrorOrWarningLog(pluginBookmarkTypes::getPropertiesProviders, BookmarksPlugin.PLUGIN_ID);
+	}
+
+	@Test
+	public void testNoErrorOrWarningLogWhileLoadingLabelProviders() {
+		assertNoErrorOrWarningLog(pluginBookmarkTypes::getLabelProviders, BookmarksPlugin.PLUGIN_ID);
+	}	
+
+	@Test
+	public void testNoErrorOrWarningLogWhileLoadingLocationProviders() {
+		assertNoErrorOrWarningLog(pluginBookmarkTypes::getLocationsProviders, BookmarksPlugin.PLUGIN_ID);
+	}	
+
+	@Test
+	public void testNoErrorOrWarningLogWhileLoadingMarkerAttributesProviders() {
+		assertNoErrorOrWarningLog(pluginBookmarkTypes::getMarkerAttributesProviders, BookmarksPlugin.PLUGIN_ID);
+	}	
+
+	@Test
+	public void testNoErrorOrWarningLogWhileLoadingGotoBookmarks() {
+		assertNoErrorOrWarningLog(pluginBookmarkTypes::getGotoBookmarks, BookmarksPlugin.PLUGIN_ID);
+	}		
+
+	@Test
+	public void testNoErrorOrWarningLogWhileLoadingImportTeamProjects() {
+		assertNoErrorOrWarningLog(pluginBookmarkTypes::getImportTeamProjects, BookmarksPlugin.PLUGIN_ID);
+	}	
+
+	@Test
+	public void testNoErrorOrWarningLogWhileLoadingBookmarkDetailsParts() {
+		assertNoErrorOrWarningLog(pluginBookmarkTypes::getBookmarkDetailParts, BookmarksPlugin.PLUGIN_ID);
+	}
+	
+	private void assertNoErrorOrWarningLog(Runnable runnable, String pluginId) {
+		List<IStatus> statuses = new ArrayList<>();
+		ILogListener logListener = (status, plugin) -> statuses.add(status);
+		WorkbenchPlugin.getDefault().getLog().addLogListener(logListener);
+		try {
+			runnable.run();
+		} finally {
+			WorkbenchPlugin.getDefault().getLog().removeLogListener(logListener);
+		}
+		assertThat(statuses.stream()
+				.filter(status -> status.getSeverity() == IStatus.ERROR || status.getSeverity() == IStatus.WARNING))
+						.allSatisfy(status -> assertThat(status.getPlugin()).isNotEqualTo(pluginId));
 	}
 
 	private PrioritizedElement<IGotoBookmark> getPrioritizedGotoBookmark(PluginBookmarkType bookmarkType,
